ce89c940bac1e0a11259cf03b72bd5c20646745f
wzqq5517992 <wzqq5517992@163.com>
2019-05-08 23:57:36 +0800

    123

---
+++ src/main/java/com/food/controller/WechatController.java
@@ -0,0 +1,16 @@
+package com.food.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wzq.Jolin
+ * @company none
+ * @create 2019-05-08 22:40
+ */
+@Controller
+@RequestMapping("/wechat")
+@Slf4j
+public class WechatController {
+}
